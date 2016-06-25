9cf8025502a7ee794438e32be3e2925321683b27
kaltemBlau <masmo.blue.gmail.com>
2016-06-25 15:30:41 +0100

    Reorganised FactorialCalculator to be self-contained.

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -10,28 +10,29 @@ namespace Recursion
     {
         static void Main(string[] args)
         {
-            string inputString;
-            int n;
-            Console.WriteLine("Calculate a factorial by entering an integer greater than or equal to zero:");
-            inputString = Console.ReadLine();
-            while (!Int32.TryParse(inputString, out n) || n < 0)
-            {
-                Console.WriteLine("You did not enter a valid number.\nPlease enter an integer greater than or equal to zero:");
-                inputString = Console.ReadLine();
-            }
             FactorialCalculator newCalculator = new FactorialCalculator();
-            int result = newCalculator.calculateFactorial(n);
-            Console.WriteLine("The factorial of {0} is {1}", n, result);
-            Console.ReadLine();
         }
     }
     class FactorialCalculator
     {
-        public int calculateFactorial(int n)
+        int inputInt;
+        int factorial;
+        public FactorialCalculator()
         {
+            inputInt = getInputInt();
+            factorial = calculateFactorial(inputInt);
+            displayResult(n: inputInt, result: factorial);
+        }
+        public FactorialCalculator(int n)
+        {
+            factorial = calculateFactorial(n);
+        }
+        private int calculateFactorial(int n)
+        {
+            //Checking for invalid values of n.
             if (n < 0)
             {
-                // May want to raise error.
+                // May want to directly raise error.
                 return -1;
             }
             if (n == 0 || n == 1)
@@ -42,6 +43,24 @@ namespace Recursion
             {
                 return n * calculateFactorial(n - 1);
             }
+        }
+        private int getInputInt()
+        {
+            string rawInput;
+            int inputInt;
+            Console.WriteLine("Calculate a factorial by entering an integer greater than or equal to zero:");
+            rawInput = Console.ReadLine();
+            while (!Int32.TryParse(rawInput, out inputInt) || inputInt < 0)
+            {
+                Console.WriteLine("You did not enter a valid number.\nPlease enter an integer greater than or equal to zero:");
+                rawInput = Console.ReadLine();
+            }
+            return inputInt;
+        }
+        private void displayResult(int n, int result)
+        {
+            Console.WriteLine("The factorial of {0} is {1}", n, result);
+            Console.ReadLine();
         }
     }
 }
