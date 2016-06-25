b816eb90f0943a50e71ebd9e6f416adb1751cfb5
kaltemBlau <masmo.blue.gmail.com>
2016-06-25 19:15:57 +0100

    Cleaning up FactorialCalculator

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -15,10 +15,19 @@ namespace Recursion
     }
     class FactorialCalculator
     {
+        string calculatorName = "Factorial Calculator";
         int inputInt;
         int factorial;
         public FactorialCalculator(bool isVerbose = false)
         {
+            /*
+             * Displays the title of the calculator.
+             * Asks the user for a integer (repeating until a valid one is supplied).
+             * Calculates the factorial (n!) of the integer.
+             * Then presents the output.
+             * Has an optional "verbose" flag for the text displayed.
+             */
+            DisplayTitle();
             inputInt = getInputInt(isVerbose);
             factorial = calculateFactorial(inputInt);
             displayResult(n: inputInt, result: factorial, verboseOutput: isVerbose);
@@ -31,18 +40,26 @@ namespace Recursion
         {
             //Checking for invalid values of n.
             if (n < 0)
-            {
                 // May want to directly raise error.
                 return -1;
-            }
             if (n == 0 || n == 1)
-            {
                 return 1;
-            }
             else
-            {
                 return n * calculateFactorial(n - 1);
+        }
+        private void DisplayTitle()
+        {
+            char emphasisCharacter = '-';
+            string emphasisLine = "";
+            //Constructing emphasis line to be as long as calculatorName.
+            for (int i = 0; i < calculatorName.Length; i++)
+            {
+                emphasisLine += emphasisCharacter;
             }
+            //Printing the title to the console.
+            Console.WriteLine(emphasisLine);
+            Console.WriteLine(calculatorName);
+            Console.WriteLine(emphasisLine);
         }
         private int getInputInt(bool verbose = false)
         {
